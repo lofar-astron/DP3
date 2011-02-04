e9a760befc401c07b3de471f5fd34ca1a96f13d1
Ger van Diepen <diepen@astron.nl>
2011-02-04 14:17:22 +0000

    bug 1638: Add a docstring

--- DPPP/src/__init__.py
+++ DPPP/src/__init__.py
@@ -5,6 +5,20 @@ import pyrap.tables as pt
 import numpy
 import pylab
 
+""" Plot NDPPP Count results
+
+    A flagging or count step in NDPPP can save the flagging percentages per
+    frequency or station. They are saved in a table (per subband) with the
+    extension ''.flagfreq'' or ''.flagstat''.
+    The flag percentages of a subband can be plotted by giving the name of
+    the table containing the results.
+
+    It is also possible to plot the results of multiple subbands by giving
+    a list of table names. Frequency results will be sorted in order of
+    frequency, while station results are averaged over the subbands.
+
+"""
+
 def plotflags (tabnames):
     t = pt.table(tabnames)
     if 'Frequency' in t.colnames():
