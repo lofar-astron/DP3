c991dcf538397afb99b11a16d32d7bcc2fb97b1d
Ger van Diepen <diepen@astron.nl>
2011-02-04 14:32:19 +0000

    bug 1638: Added copyright

--- DPPP/src/__init__.py
+++ DPPP/src/__init__.py
@@ -1,3 +1,26 @@
+# __init__.py: Top level .py file for DPPP flagging results plotting
+#
+# Copyright (C) 2007
+# ASTRON (Netherlands Institute for Radio Astronomy)
+# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
+#
+# This file is part of the LOFAR software suite.
+# The LOFAR software suite is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# The LOFAR software suite is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with the LOFAR software suite. If not, see <http://www.gnu.org/licenses/>.
+#
+# $Id$
+
+
 # Plot the NDPPP count results by frequency or baseline.
 # Frequencies are concatenated; baselines are averaged.
 
@@ -5,17 +28,17 @@ import pyrap.tables as pt
 import numpy
 import pylab
 
-""" Plot NDPPP Count results
+"""Plot NDPPP Count results
 
-    A flagging or count step in NDPPP can save the flagging percentages per
-    frequency or station. They are saved in a table (per subband) with the
-    extension ''.flagfreq'' or ''.flagstat''.
-    The flag percentages of a subband can be plotted by giving the name of
-    the table containing the results.
+A flagging or count step in NDPPP can save the flagging percentages per
+frequency or station. They are saved in a table (per subband) with the
+extension ''.flagfreq'' or ''.flagstat''.
+The flag percentages of a subband can be plotted by giving the name of
+the table containing the results.
 
-    It is also possible to plot the results of multiple subbands by giving
-    a list of table names. Frequency results will be sorted in order of
-    frequency, while station results are averaged over the subbands.
+It is also possible to plot the results of multiple subbands by giving
+a list of table names. Frequency results will be sorted in order of
+frequency, while station results are averaged over the subbands.
 
 """
 
