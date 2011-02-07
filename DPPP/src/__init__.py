c9efea0fb73f8e7c0af57f98c13b1189e46561ef
Ger van Diepen <diepen@astron.nl>
2011-02-07 07:15:52 +0000

    bug 1638: Improved docstrings

--- DPPP/src/__init__.py
+++ DPPP/src/__init__.py
@@ -24,25 +24,26 @@
 # Plot the NDPPP count results by frequency or baseline.
 # Frequencies are concatenated; baselines are averaged.
 
+""" Module to plot NDPPP count results. """
+
 import pyrap.tables as pt
 import numpy
 import pylab
 
-"""Plot NDPPP Count results
-
-A flagging or count step in NDPPP can save the flagging percentages per
-frequency or station. They are saved in a table (per subband) with the
-extension ''.flagfreq'' or ''.flagstat''.
-The flag percentages of a subband can be plotted by giving the name of
-the table containing the results.
-
-It is also possible to plot the results of multiple subbands by giving
-a list of table names. Frequency results will be sorted in order of
-frequency, while station results are averaged over the subbands.
-
-"""
-
 def plotflags (tabnames):
+    """Plot NDPPP Count results
+
+    A flagging or count step in NDPPP can save the flagging percentages per
+    frequency or station. They are saved in a table (per subband) with the
+    extension ''.flagfreq'' or ''.flagstat''.
+    The flag percentages of a subband can be plotted by giving the name of
+    the table containing the results.
+
+    It is also possible to plot the results of multiple subbands by giving
+    a list of table names. Frequency results will be sorted in order of
+    frequency, while station results are averaged over the subbands.
+
+    """
     t = pt.table(tabnames)
     if 'Frequency' in t.colnames():
         t1 = t.sort ('Frequency')
@@ -55,5 +56,5 @@ def plotflags (tabnames):
             names.append (t1.getcell('Name', 0))
         pylab.plot (numpy.array(percs), '+')
     else:
-        raise 'Table appears not to be a NDPPP Count result; it does not contain a Frequency or Station column'
+        raise RuntimeError('Table appears not to be a NDPPP Count result; it does not contain a Frequency or Station column')
 
