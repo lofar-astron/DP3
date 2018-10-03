f3608215f7ace4b9a1fd8b569ba3a9688a474d80
André Offringa <offringa@gmail.com>
2018-10-03 09:54:21 +0200

    Replace casacore::CountedPtrs with unique_ptrs, which are faster and simpler (#46)

--- DPPP/MSUpdater.cc
+++ DPPP/MSUpdater.cc
@@ -84,7 +84,7 @@ namespace DP3 {
       if (itsStManKeys.stManName == "dysco" && itsStManKeys.dyscoDataBitRate != 0) {
         casacore::Record dyscoSpec = itsStManKeys.GetDyscoSpec();
         DataManagerCtor dyscoConstructor = DataManager::getCtor("DyscoStMan");
-        CountedPtr<DataManager> dyscoStMan(dyscoConstructor(colName + "_dm", dyscoSpec));
+        std::unique_ptr<DataManager> dyscoStMan(dyscoConstructor(colName + "_dm", dyscoSpec));
         ColumnDesc directColumnDesc(cd);
         directColumnDesc.setOptions(casacore::ColumnDesc::Direct | casacore::ColumnDesc::FixedShape);
         TableDesc td;
