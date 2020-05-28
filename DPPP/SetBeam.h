77c3b94c55dd6d17d8ff6d2b0519af6032c3af95
André Offringa <offringa@gmail.com>
2020-05-28 13:08:05 +0200

    Add step for setting beam keywords
    This is useful after a predict (with DP3 or image based with e.g. wsclean) in which no consideration of the beam was applied.
    In that case, the visibilities are "corrected" for
    the beam towards an arbitrary direction. If one source is predicted, it is as if the beam is corrected for in that direction.
    This step makes that possible, and e.g. to unapply/corrupt for the beam after having predicted/fted the visibilities afterwards.
    
    This fixes #236.

---
+++ DPPP/SetBeam.h
@@ -0,0 +1,44 @@
+#ifndef DPPP_SETBEAM_H
+#define DPPP_SETBEAM_H
+
+// @file
+// @brief DPPP step class to set the beam keywords in a ms
+
+#include "DPInput.h"
+#include "DPBuffer.h"
+#include "Position.h"
+
+#include <casacore/measures/Measures/MDirection.h>
+
+namespace DP3 {
+
+class ParameterSet;
+
+namespace DPPP {
+  
+class SetBeam final : public DPStep
+{
+public:
+  // Parameters are obtained from the parset using the given prefix.
+  SetBeam (DPInput* input, const ParameterSet& parameters, const string& prefix);
+
+  bool process(const DPBuffer& buffer) override;
+
+  void finish() override { };
+
+  void updateInfo(const DPInfo& info) override;
+
+  void show(std::ostream&) const override;
+  
+private:
+  DPInput* _input;
+  string _name;
+  std::vector<string> _directionStr;
+  casacore::MDirection _direction;
+  BeamCorrectionMode _mode;
+};
+
+} } //# end namespaces
+
+#endif
+
