ee693c5e08d218e1a5e3667badcf262d3d047556
Lars Krombeen <lars.krombeen@cgi.com>
2020-06-24 10:50:50 +0200

    Move CMakeLists; Clean up files

--- doc/conf.py
+++ doc/conf.py
@@ -122,35 +122,6 @@ html_sidebars = {
 htmlhelp_basename = 'dpppdoc'
 
 
-# -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'dppp.tex', 'dppp Documentation',
-     'l', 'manual'),
-]
-
-
 # -- Options for manual page output ---------------------------------------
 
 # One entry per manual page. List of tuples
@@ -159,18 +130,3 @@ man_pages = [
     (master_doc, 'dppp', 'dppp Documentation',
      [author], 1)
 ]
-
-
-# -- Options for Texinfo output -------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'dppp', 'dppp Documentation',
-     author, 'dppp', 'One line description of project.',
-     'Miscellaneous'),
-]
-
-
-
