1aaeb6ecd25ebe9b7e0b32da58c1321dbb462d89
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2020-09-10 12:39:02 +0200

    Add reference predictions to IDG test.

--- DDECal/test/integration/tIDGPredict.sh
+++ DDECal/test/integration/tIDGPredict.sh
@@ -9,12 +9,24 @@ if [ ! -f $INIT ]; then
   exit 1;
 fi
 source $INIT
-gunzip -c -f $srcdir/foursources.fits.gz > foursources.fits
+gunzip -c -f $srcdir/foursources.fits.gz > foursources-model.fits
 cp $srcdir/foursources.reg .
 
-echo "IDG calibration using four sources"
+# Create expected taql output.
+echo "    select result of 0 rows" > taql.ref
+
+#Use wsclean for generating a reference prediction, in the MODEL_DATA column.
+wsclean -predict -name foursources tDDECal.MS
+
+echo "Predict four point sources using IDG"
 cmd="$dpppexe checkparset=1 msin=tDDECal.MS msout=.\
   steps=[ddecal] ddecal.useidg=True ddecal.idg.regions=foursources.reg\
-  ddecal.idg.images=[foursources.fits]"
+  ddecal.idg.images=[foursources-model.fits]\
+  ddecal.onlypredict=True msout.datacolumn=IDG_DATA"
 echo $cmd
 $cmd >& /dev/null
+
+cmd="$taqlexe 'select MODEL_DATA,IDG_DATA from tDDECal.MS where not all(near(MODEL_DATA,IDG_DATA,1e-3))'
+echo $cmd
+eval $cmd
+diff taql.out taql.ref || exit 1
