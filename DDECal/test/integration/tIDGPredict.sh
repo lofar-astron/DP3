233e935796febd334322f10a4d3fa1e663824ccc
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2020-09-25 10:52:49 +0200

    Add foursources_DATA column to tDDECal.MS.
    tIDGPredict can now run on CI.
    Single-pixel tests only run locally if foursources test fails.
    Remove unused tDDECalBL.in_MS.tgz

--- DDECal/test/integration/tIDGPredict.sh
+++ DDECal/test/integration/tIDGPredict.sh
@@ -16,8 +16,6 @@ cp $srcdir/resources/*.reg .
 # Create expected taql output.
 echo "    select result of 0 rows" > taql.ref
 
-$taqlexe 'update tDDECal.MS set WEIGHT_SPECTRUM=1, FLAG=False'
-
 passed=0
 failed=0
 skipped=0
@@ -31,7 +29,7 @@ compare_results() {
   # Ignore baselines with antennna 6 for now, since not all predictors
   # generate visibilities for those baselines.
   # TODO (AST-223): Investigate what the expected behavior is.
-  $taqlexe 'select from tDDECal.MS where ANTENNA2 != 6 and not all(near(MODEL_DATA,IDG_DATA,1e-3))' > taql.out
+  $taqlexe "select from tDDECal.MS where ANTENNA2 != 6 and not all(near($1_DATA,IDG_DATA,1e-3))" > taql.out
   echo -n "Predict source: $1 offset: $2 result: "
   if diff -q taql.out taql.ref; then
     echo "${text_boldgreen}Test passed.${text_normal}"
@@ -43,7 +41,12 @@ compare_results() {
 }
 
 # Test an input with four sources.
-wsclean -use-idg -predict -name foursources tDDECal.MS
+
+# Since wsclean on CI does not support IDG, tDDECal.MS has a foursources_DATA
+# column with the reference output/visibilities for foursources-model.fits.
+# These commands generated the column:
+#wsclean -use-idg -predict -name resources/foursources tDDECal.MS
+#taql "alter table tDDECal.MS rename column MODEL_DATA to foursources_DATA"
 
 echo "Predict four sources using IDG"
 cmd="$dpppexe checkparset=1 msin=tDDECal.MS msout=.\
@@ -55,28 +58,34 @@ $cmd
 compare_results foursources "(multiple)"
 
 # Test inputs that contain a single source.
-SOURCES="center ra dec radec"
-OFFSETS="center dl dm dldm"
-for source in $SOURCES; do
-  #Use wsclean for generating a reference prediction, in the MODEL_DATA column.
-  wsclean -use-idg -predict -name $source tDDECal.MS
+# Since these tests take quite some time, they only run locally, and only
+# if the foursources test fails or if it is commented out.
+# CI runs don't work since wsclean does not support IDG on CI.
+if [ $passed != 1 -a -z "$CI" ]; then
+  SOURCES="center ra dec radec"
+  OFFSETS="center dl dm dldm"
+  for source in $SOURCES; do
+    #Generate reference predictions in the ${source}_DATA column.
+    wsclean -use-idg -predict -name $source tDDECal.MS
+    taql "alter table tDDECal.MS rename column MODEL_DATA to ${source}_DATA"
 
-  for offset in $OFFSETS; do
-    echo "Predict source: $source offset: $offset using IDG"
-    if grep -q "^polygon" $source-$offset.reg; then
-      cmd="$dpppexe checkparset=1 msin=tDDECal.MS msout=.\
-        steps=[ddecal] ddecal.useidg=True ddecal.idg.regions=$source-$offset.reg\
-        ddecal.idg.images=[$source-model.fits]\
-        ddecal.onlypredict=True msout.datacolumn=IDG_DATA"
-      echo $cmd
-      $cmd
-      compare_results $source $offset
-    else
-      echo "${text_boldcyan}Test skipped: No polygon(s) defined!${text_normal}"
-      skipped=$((skipped + 1))
-    fi
+    for offset in $OFFSETS; do
+      echo "Predict source: $source offset: $offset using IDG"
+      if grep -q "^polygon" $source-$offset.reg; then
+        cmd="$dpppexe checkparset=1 msin=tDDECal.MS msout=.\
+          steps=[ddecal] ddecal.useidg=True ddecal.idg.regions=$source-$offset.reg\
+          ddecal.idg.images=[$source-model.fits]\
+          ddecal.onlypredict=True msout.datacolumn=IDG_DATA"
+        echo $cmd
+        $cmd
+        compare_results $source $offset
+      else
+        echo "${text_boldcyan}Test skipped: No polygon(s) defined!${text_normal}"
+        skipped=$((skipped + 1))
+      fi
+    done
   done
-done
+fi
 
 echo Tests passed: $passed skipped: $skipped failed: $failed
 exit $failed
