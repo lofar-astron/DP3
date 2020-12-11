d3c01550ef1e649c9613bb3461cf8f4a8824aa5c
Sebastiaan van der Tol <tol@astron.nl>
2020-12-11 09:12:59 +0100

    Re-enable python step, now using pybind11 instead of boost::python

---
+++ PythonDPPP/PyDPStep.cc
@@ -0,0 +1,104 @@
+#include "PyDPStep.h"
+#include "PyDPStepImpl.h"
+#include "../DPPP/DPRun.h"
+
+#include <iostream>
+#include <pybind11/pybind11.h>
+#include <pybind11/embed.h> // everything needed for embedding
+
+// Define the function to make the PythonStep 'constructor' known.
+extern "C" {
+void register_pythondppp() {
+  DP3::DPPP::DPRun::registerStepCtor("pythondppp",
+                                     DP3::DPPP::PyDPStep::create_instance);
+}
+}
+
+namespace DP3 {
+namespace DPPP {
+
+void PyDPStepImpl ::show (std::ostream& os) const
+{
+    // TODO redirect sys.stdout to os
+
+    pybind11::gil_scoped_acquire gil;  // Acquire the GIL while in this scope.
+    // Try to look up the overloaded method on the Python side.
+    pybind11::function overload = pybind11::get_overload(this, "show");
+    if (overload) overload();  // Call the Python function.
+}
+
+bool PyDPStepImpl::process (const DPBuffer& bufin)
+{
+    m_count++;
+
+    auto dpbuffer = std::shared_ptr<DPBuffer>(new DPBuffer(bufin));
+
+    // fetch optional data
+    if (m_fetch_uvw)
+    {
+        m_input->fetchUVW(bufin, *dpbuffer, m_timer);
+    }
+
+    if (m_fetch_weights)
+    {
+        m_input->fetchWeights(bufin, *dpbuffer, m_timer);
+    }
+
+    PYBIND11_OVERLOAD_PURE(
+        bool,           /* Return type */
+        DPStepWrapper,  /* Parent class */
+        process,        /* Name of function in C++ (must match Python name) */
+        dpbuffer        /* Argument(s) */
+    );
+}
+
+void PyDPStepImpl::finish ()
+{
+    PYBIND11_OVERLOAD_PURE(
+        void,           /* Return type */
+        DPStepWrapper,  /* Parent class */
+        finish          /* Name of function in C++ (must match Python name) */
+    );
+}
+
+void PyDPStepImpl::updateInfo (const DPInfo& dpinfo)
+{
+    PYBIND11_OVERLOAD_NAME(
+        void,          /* Return type */
+        DPStepWrapper, /* Parent class */
+        "update_info",
+        updateInfo,    /* Name of function in C++ (must match Python name) */
+        dpinfo         /* Argument(s) */
+    );
+}
+
+
+
+void PyDPStepImpl::hold()
+{
+    m_py_object = py::cast(static_cast<DPStep*>(this));
+}
+
+DPStep::ShPtr PyDPStep::create_instance(
+    DPInput* input,
+    const ParameterSet& parset,
+    const string& prefix)
+{
+    std::string module_name = parset.getString(prefix + "python.module");
+    std::string class_name = parset.getString(prefix + "python.class");
+
+    static py::scoped_interpreter guard{}; // start the interpreter and keep it alive
+    static py::module mydpstep_module = py::module::import(module_name.c_str());
+    
+    auto pydpstep_instance = mydpstep_module.attr(class_name.c_str())(parset, prefix);
+    auto pydpstep_instance_ptr = pydpstep_instance.cast<std::shared_ptr<PyDPStepImpl>>();
+    pydpstep_instance_ptr->hold();
+    pydpstep_instance_ptr->set_input(input);
+//    pydpstep_instance_ptr->set_parset(parset);
+//    pydpstep_instance_ptr->set_name(prefix);
+
+    return pydpstep_instance_ptr;
+}
+
+} // namespace DPPP
+} // namespace DP3
---
+++ PythonDPPP/PyDPStepImpl.h
@@ -0,0 +1,89 @@
+#ifndef DPPP_PYDPSTEPIMPL_H
+#define DPPP_PYDPSTEPIMPL_H
+
+#include "PyDPStep.h"
+#include "../DPPP/DPInput.h"
+
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+namespace DP3 {
+namespace DPPP {
+
+class DPStepWrapper : public DPStep
+{
+public:
+    using DPStep::DPStep;
+    using DPStep::updateInfo;
+    using DPStep::info;
+
+    using DPStep::getNextStep;
+
+    DPStep::ShPtr get_next_step()
+    {
+//         const DPStep::ShPtr &result = getNextStep();
+//         return result;
+        return DPStep::ShPtr(getNextStep());
+    }
+
+    bool process_next_step (const DPBuffer& dpbuffer)
+    {
+        return get_next_step()->process(dpbuffer);
+    }
+
+
+    int get_count() {return m_count;}
+    void set_input(DPInput* input) {m_input = input;};
+    void set_parset(const ParameterSet& parset) {m_parset = parset;};
+    void set_name(const string& name) {m_name = name;};
+
+    bool m_fetch_uvw = false;
+    bool m_fetch_weights = false;
+
+//     casacore::Cube<casacore::Complex>& get_data() {return m_dpbuffer.getData();}
+//
+//     casacore::Cube<float>& get_weights()
+//     {
+//         m_input->fetchWeights(*m_dpbuffer_in, m_dpbuffer, m_timer);
+//         return m_dpbuffer.getWeights();
+//     }
+//
+//     casacore::Matrix<double>& get_uvw()
+//     {
+//         m_input->fetchUVW(*m_dpbuffer_in, m_dpbuffer, m_timer);
+//         return m_dpbuffer.getUVW();
+//     }
+
+protected:
+    int m_count = 0;
+    DPInput* m_input;
+    ParameterSet m_parset;
+    string m_name;
+    const DPBuffer *m_dpbuffer_in;
+    NSTimer m_timer;
+};
+
+class PyDPStepImpl : public DPStepWrapper {
+public:
+
+    using DPStepWrapper::DPStepWrapper;
+
+    virtual void show (std::ostream& os) const override;
+
+    virtual void updateInfo (const DPInfo&) override;
+
+    virtual bool process (const DPBuffer&) override;
+
+    // Finish the processing of this step and subsequent steps.
+    virtual void finish() override;
+
+    void hold();
+
+private:
+    py::object m_py_object;
+};
+
+} // namespace DPPP
+} // namespace DP3
+
+#endif
