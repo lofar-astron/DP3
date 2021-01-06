61981c059f753697363f7f90adb22d79fe7114e9
Sebastiaan van der Tol <tol@astron.nl>
2021-01-06 10:22:08 +0100

    clang-format

---
+++ PythonDPPP/test/testmydpstep.cc
@@ -0,0 +1,7 @@
+#include "PyDPStep.h"
+#include <iostream>
+
+int main() {
+  //     DP3::DPPP::DPStep::ShPtr mydpstep_ptr =
+  //     DP3::DPPP::PyDPStep::create_instance(); mydpstep_ptr->show(std::cout);
+}
