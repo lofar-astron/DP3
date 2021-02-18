ace4639f1bafbfd3eab894b4256deb871d6cc5c8
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2021-02-18 13:11:34 +0100

    Handle defined 0-dimensional casacore arrays.
    When only using isDefined(), casacore internally allocates
    0 bytes for an Array but it may read the first byte.

--- DPPP/MS.cc
+++ DPPP/MS.cc
@@ -25,29 +25,17 @@ const std::string kSingleFactorPerBL = "SINGLE_FACTOR_PER_BASELINE";
 /// BDA_FACTORS table.
 const std::string kBDAFactorsTable = "BDA_FACTORS";
 const std::string kFactor = "FACTOR";
-
-/// SPECTRAL_WINDOW table.
-const std::string kSpectralWindowTable = "SPECTRAL_WINDOW";
 const std::string kSpectralWindowId = "SPECTRAL_WINDOW_ID";
-const std::string kDataDescTable = "DATA_DESCRIPTION";
+
+/// SPECTRAL_WINDOW table extensions.
+const std::string kSpectralWindowTable = "SPECTRAL_WINDOW";
 const std::string kBDAFreqAxisId = "BDA_FREQ_AXIS_ID";
 const std::string kBDASetId = "BDA_SET_ID";
-const std::string kChanFreq = "CHAN_FREQ";
-const std::string kChanWidth = "CHAN_WIDTH";
-const std::string kEffectiveBW = "EFFECTIVE_BW";
-const std::string kResolution = "RESOLUTION";
-const std::string kNumChan = "NUM_CHAN";
-const std::string kTotalBandWidth = "TOTAL_BANDWIDTH";
-const std::string kRefFrequency = "REF_FREQUENCY";
 
 const std::string kLofarAntennaSet = "LOFAR_ANTENNA_SET";
 
 const std::string kAntennaTable = "ANTENNA";
-const std::string kName = "NAME";
-const std::string kDishDiameter = "DISH_DIAMETER";
-const std::string kPosition = "POSITION";
-const std::string kMeasFreqRef = "MEAS_FREQ_REF";
-
+const std::string kDataDescTable = "DATA_DESCRIPTION";
 const std::string kObservationTable = "OBSERVATION";
 
 }  // namespace DP3MS
