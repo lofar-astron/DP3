dc948bfd271c5bea88a2a3c0c9a773483133e898
Lars Krombeen <lars.krombeen@cgi.com>
2021-02-22 08:07:55 +0100

    Fix DDECal with columreader

--- DPPP/ColumnReader.cc
+++ DPPP/ColumnReader.cc
@@ -15,7 +15,8 @@ ColumnReader::ColumnReader(DPInput& input, const ParameterSet& parset,
     : input_(input),
       name_(prefix),
       column_name_(parset.getString(prefix + "column", column)),
-      operation_(parset.getString(prefix + "operation", "replace")) {
+      operation_(parset.getString(prefix + "operation", "replace")),
+      buffer_() {
   if (operation_ != "subtract" && operation_ != "add" &&
       operation_ != "replace") {
     throw std::invalid_argument("Invalid ColumnReader operation " + operation_);
@@ -23,17 +24,17 @@ ColumnReader::ColumnReader(DPInput& input, const ParameterSet& parset,
 }
 
 bool ColumnReader::process(const DPBuffer& buffer) {
-  DPBuffer buffer_model = buffer;
+  buffer_.copy(buffer);
   ArrayColumn<casacore::Complex> model_col(input_.table(), column_name_);
-  model_col.getColumnCells(buffer.getRowNrs(), buffer_model.getData());
+  model_col.getColumnCells(buffer.getRowNrs(), buffer_.getData());
 
   if (operation_ == "add") {
-    buffer_model.setData(buffer.getData() + buffer_model.getData());
+    buffer_.setData(buffer.getData() + buffer_.getData());
   } else if (operation_ == "subtract") {
-    buffer_model.setData(buffer.getData() - buffer_model.getData());
+    buffer_.setData(buffer.getData() - buffer_.getData());
   }
 
-  getNextStep()->process(buffer_model);
+  getNextStep()->process(buffer_);
 
   return false;
 }
