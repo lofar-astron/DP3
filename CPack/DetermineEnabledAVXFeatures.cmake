bbcc25f64f0ec30b289d201ab45a49ca7f04f6fc
Marcel Loose <loose@astron.nl>
2021-03-08 13:55:18 +0100

    Re-use USE_AVX variables instead of new ENABLE_AVX
    We can re-use the USE_AVX* variables that are set by DetermineEnabledAVXFeatures.cmake.
    This also improves processing speed (slightly) when reconfiguring.

--- CPack/DetermineEnabledAVXFeatures.cmake
+++ CPack/DetermineEnabledAVXFeatures.cmake
@@ -13,29 +13,34 @@ the option ``PORTABLE`` to ``TRUE``. In that case, all cached
 ``USE_<AVX-feature>`` variables will be removed from the cache.
 #]=======================================================================]
 
-# If we're building portable code, remove every cached USE_<AVX> variable.
-if(PORTABLE)
-  get_cmake_property(_cache_variables CACHE_VARIABLES)
-  foreach(_var ${_cache_variables})
-    if(_var MATCHES "USE_AVX")
+get_cmake_property(_cache_variables CACHE_VARIABLES)
+foreach(_var ${_cache_variables})
+  if(_var MATCHES "^USE_AVX")
+    message("Checking variable '${_var}'")
+    if(PORTABLE)
+      # We're building portable code, remove cached USE_AVX* variable
+      message(STATUS "Removing '${_var}' from the cache")
       unset(${_var} CACHE)
+    elseif(${_var})
+      # AVX-features were already determined or set explicitly. Bail out
+      message(STATUS "Flag '${_var} was enabled. Bailing out")
+      return()
     endif()
-  endforeach()
-else()
-  get_directory_property(_compile_options COMPILE_OPTIONS)
-  string(REPLACE ";" " " _compile_options "${_compile_options}")
-  execute_process(COMMAND bash -c 
-    "echo | ${CMAKE_CXX_COMPILER} ${_compile_options} -E -v - 2>&1 | grep cc1"
-    OUTPUT_VARIABLE _compile_flags)
-  # Search for enabled AVX-features: GCC uses -m<feature> Clang +<feature>.
-  # Strip off the "-m" or "+" prefix to get the bare feature.
-  set(_pattern "(-m|\\+)(avx[0-9a-z]*)")
-  string(REGEX MATCHALL "${_pattern}" _avx_features "${_compile_flags}")
-  string(REGEX REPLACE "${_pattern}" "\\2" _avx_features "${_avx_features}")
-  foreach(_feature ${_avx_features})
-    string(TOUPPER ${_feature} _FEATURE)
-    set(USE_${_FEATURE} ON CACHE BOOL "Use ${_FEATURE}" FORCE)
-    mark_as_advanced(USE_${_FEATURE})
-  endforeach()
-endif()
+  endif()
+endforeach()
+get_directory_property(_compile_options COMPILE_OPTIONS)
+string(REPLACE ";" " " _compile_options "${_compile_options}")
+execute_process(COMMAND bash -c 
+  "echo | ${CMAKE_CXX_COMPILER} ${_compile_options} -E -v - 2>&1 | grep cc1"
+  OUTPUT_VARIABLE _compile_flags)
+# Search for enabled AVX-features: GCC uses -m<feature> Clang +<feature>.
+# Strip off the "-m" or "+" prefix to get the bare feature.
+set(_pattern "(-m|\\+)(avx[0-9a-z]*)")
+string(REGEX MATCHALL "${_pattern}" _avx_features "${_compile_flags}")
+string(REGEX REPLACE "${_pattern}" "\\2" _avx_features "${_avx_features}")
+foreach(_feature ${_avx_features})
+  string(TOUPPER ${_feature} _FEATURE)
+  set(USE_${_FEATURE} ON CACHE BOOL "Use ${_FEATURE}" FORCE)
+  mark_as_advanced(USE_${_FEATURE})
+endforeach()
 
