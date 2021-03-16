a63d0bb66af3f5bcbfe5809c2eeedc61c92f239f
Tammo Jan Dijkema <dijkema@astron.nl>
2021-03-16 21:59:54 +0100

    Remove unused files
    These tests were replaced by the ones in DPPP/test/unit/

--- PythonDPPP/test/testmydpstep.cc
+++
@@ -1,7 +0,0 @@
-#include "PyDPStep.h"
-#include <iostream>
-
-int main() {
-  //     DP3::DPPP::DPStep::ShPtr mydpstep_ptr =
-  //     DP3::DPPP::PyDPStep::create_instance(); mydpstep_ptr->show(std::cout);
-}
