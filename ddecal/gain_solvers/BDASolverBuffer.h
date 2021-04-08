d3d7fc08f67bed6fa8ccb84417a23c506f11583d
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2021-04-08 15:33:05 +0200

    Add initial BDASolverBuffer with AssignAndWeight.

---
+++ ddecal/gain_solvers/BDASolverBuffer.h
@@ -0,0 +1,44 @@
+// Copyright (C) 2020 ASTRON (Netherlands Institute for Radio Astronomy)
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef DDECAL_BDA_SOLVER_BUFFER_H
+#define DDECAL_BDA_SOLVER_BUFFER_H
+
+#include <complex>
+#include <vector>
+
+namespace dp3 {
+namespace base {
+
+class BDABuffer;
+
+class BDASolverBuffer {
+ public:
+  BDASolverBuffer() : data_(), model_buffers_() {}
+
+  /**
+   * This function takes (unweighted) data and model data, as well as a
+   * weights array, weights these and initializes the buffer from
+   * them.
+   *
+   * @param data_buffers A vector with buffers that cover the solution interval.
+   * Each buffer should contain unweighted data and the corresponding weight
+   * values.
+   * @param model_buffers Buffers with model data. For each direction,
+   * model_buffers holds a vector with buffers with the unweighted model
+   * data. The BDA layout of the buffer in the inner vector should match the
+   * layout of 'data_buffers'. The BDASolverBuffer takes ownership of the model
+   * buffers.
+   */
+  void AssignAndWeight(const std::vector<BDABuffer>& data_buffers,
+                       std::vector<std::vector<BDABuffer>>&& model_buffers);
+
+ private:
+  std::vector<BDABuffer> data_;
+  std::vector<std::vector<BDABuffer>> model_buffers_;
+};
+
+}  // namespace base
+}  // namespace dp3
+
+#endif  // DDECAL_BDA_SOLVER_BUFFER_H
