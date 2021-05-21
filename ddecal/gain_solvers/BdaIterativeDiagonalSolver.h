d430ecbe2eeb69fcb64f995c5191b493b7699f84
Andre Offringa <offringa@astron.nl>
2021-05-21 18:27:16 +0000

    Add BDA iterative diagonal solver

--- ddecal/gain_solvers/BdaIterativeDiagonalSolver.h
+++ ddecal/gain_solvers/BdaIterativeDiagonalSolver.h
@@ -1,8 +1,8 @@
 // Copyright (C) 2020 ASTRON (Netherlands Institute for Radio Astronomy)
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#ifndef DDECAL_BDA_ITERATIVE_SCALAR_SOLVER_H
-#define DDECAL_BDA_ITERATIVE_SCALAR_SOLVER_H
+#ifndef DDECAL_BDA_ITERATIVE_DIAGONAL_SOLVER_H
+#define DDECAL_BDA_ITERATIVE_DIAGONAL_SOLVER_H
 
 #include "BdaSolverBase.h"
 #include "SolveData.h"
@@ -10,13 +10,13 @@
 namespace dp3 {
 namespace ddecal {
 
-class BdaIterativeScalarSolver final : public BdaSolverBase {
+class BdaIterativeDiagonalSolver final : public BdaSolverBase {
  public:
   SolveResult Solve(const SolveData& data,
                     std::vector<std::vector<DComplex>>& solutions, double time,
                     std::ostream* stat_stream) override;
 
-  size_t NSolutionPolarizations() const override { return 1; }
+  size_t NSolutionPolarizations() const override { return 2; }
 
  private:
   void PerformIteration(const SolveData::ChannelBlockData& cb_data,
@@ -39,4 +39,4 @@ class BdaIterativeScalarSolver final : public BdaSolverBase {
 }  // namespace ddecal
 }  // namespace dp3
 
-#endif  // DDECAL_BDA_ITERATIVE_SCALAR_SOLVER_H
+#endif  // DDECAL_BDA_ITERATIVE_DIAGONAL_SOLVER_H
