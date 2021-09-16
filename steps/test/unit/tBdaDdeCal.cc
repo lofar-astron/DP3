c70acfec32e053f35f1e252c5d0f31b9728f1c99
Mark de Wever <koraq@xs4all.nl>
2021-09-16 17:04:09 +0200

    Add unit tests
    Tests are added for:
    - DDECal::show()
    - BdaDdeCal::show()

---
+++ steps/test/unit/tBdaDdeCal.cc
@@ -0,0 +1,88 @@
+// Copyright (C) 2020 ASTRON (Netherlands Institute for Radio Astronomy)
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <boost/test/unit_test.hpp>
+
+#include "tStepCommon.h"
+#include "../../BdaDdeCal.h"
+#include "../../InputStep.h"
+
+BOOST_AUTO_TEST_SUITE(bdaddecal)
+
+/// Helper forwarder to improve test failure output.
+void TestShow(std::string expected,
+              std::vector<std::pair<std::string, std::string>> parameters) {
+  BOOST_CHECK_EQUAL(expected,
+                    dp3::steps::test::Show<dp3::steps::BdaDdeCal>(parameters));
+}
+
+void TestShowDefault() {
+  TestShow(
+      R"(BdaDdeCal prefix.
+  mode (constraints):  diagonal
+  directions:          [[center]]
+  solver algorithm:    directionsolve
+  H5Parm:              tDDECal.MS/instrument.h5
+  subtract model:      false
+  solution interval:   0 s
+  #channels/block:     1
+  #channel blocks:     18446744073709551615
+  tolerance:           0.0001
+  max iter:            50
+  flag unconverged:    false
+     diverged only:    false
+  propagate solutions: false
+       converged only: false
+  detect stalling:     true
+  step size:           0.2
+Model steps for direction [center]
+BdaPredict prefix.
+Using a regular predict per baseline group
+
+)",
+      std::vector<std::pair<std::string, std::string>>{
+          {"msin", "tDDECal.MS"}, {"prefix.directions", "[[center]]"}});
+}
+
+void TestShowModified() {
+  TestShow(
+      R"(BdaDdeCal prefix.
+  mode (constraints):  diagonal
+  directions:          [[center]]
+  solver algorithm:    hybrid
+  H5Parm:              tDDECal.MS/instrument.h5
+  subtract model:      true
+  solution interval:   0 s
+  #channels/block:     44
+  #channel blocks:     18446744073709551615
+  tolerance:           1e-05
+  max iter:            49
+  flag unconverged:    true
+     diverged only:    true
+  propagate solutions: true
+       converged only: true
+  detect stalling:     true
+  step size:           0.2
+Model steps for direction [center]
+BdaPredict prefix.
+Using a regular predict per baseline group
+
+)",
+      std::vector<std::pair<std::string, std::string>>{
+          {"msin", "tDDECal.MS"},
+          {"prefix.directions", "[[center]]"},
+          {"prefix.propagatesolutions", "true"},
+          {"prefix.propagateconvergedonly", "true"},
+          {"prefix.flagunconverged", "true"},
+          {"prefix.flagdivergedonly", "true"},
+          {"prefix.subtract", "true"},
+          {"prefix.solveralgorithm", "hybrid"},
+          {"prefix.nchan", "44"},
+          {"prefix.maxiter", "49"}});
+}
+
+BOOST_AUTO_TEST_CASE(test_show) {
+  TestShowDefault();
+  TestShowModified();
+}
+BOOST_AUTO_TEST_SUITE_END()
--- steps/test/unit/tStepCommon.h
+++ steps/test/unit/tStepCommon.h
@@ -7,6 +7,8 @@
 #define DPPP_STEPS_TEST_UNIT_TSTEPCOMMON_H
 
 #include "../../Step.h"
+#include "../../InputStep.h"
+#include "../../../common/ParameterSet.h"
 #include <vector>
 
 namespace dp3 {
@@ -15,6 +17,32 @@ namespace test {
 
 /// Connect a series of steps and execute them.
 void Execute(const std::vector<std::shared_ptr<Step>>& steps);
+
+/**
+ * Helper function to aid testing @c Step::show.
+ *
+ * Creates a @a Step using @a parameters for its parset and a fixed prefix
+ * @c "prefix.". It returns the result of @a Step::show.
+ */
+template <class Step>
+inline std::string Show(
+    const std::vector<std::pair<std::string, std::string>>& parameters) {
+  class : public dp3::steps::InputStep {
+    void finish() override {}
+    void show(std::ostream&) const override {}
+  } input;
+
+  dp3::common::ParameterSet parset;
+  for (const auto& parameter : parameters)
+    parset.add(parameter.first, parameter.second);
+
+  const Step step{&input, parset, "prefix."};
+  std::stringstream output;
+  // Ensure the test doesn't depend on the system's locale settings.
+  output.imbue(std::locale::classic());
+  step.show(output);
+  return output.str();
+}
 
 }  // namespace test
 }  // namespace steps
