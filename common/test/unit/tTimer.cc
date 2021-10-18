5e4e3faa10c371cf86c4105305d13465e5ac8f9e
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2021-10-18 14:40:12 +0000

    Remove multithreaded accumulator test
    Use a MockAccumulator with operator+= instead.

--- common/test/unit/tTimer.cc
+++ common/test/unit/tTimer.cc
@@ -1,16 +1,36 @@
-// Copyright (C) 2020 ASTRON (Netherlands Institute for Radio Astronomy)
+// Copyright (C) 2021 ASTRON (Netherlands Institute for Radio Astronomy)
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "../../Timer.h"
 
 #include <boost/test/unit_test.hpp>
 
-#include <algorithm>
-#include <atomic>
-#include <numeric>
-#include <regex>
-#include <thread>
-#include <type_traits>
+#include <deque>
+#include <vector>
+
+namespace {
+
+/// Global queue with the time points that MockClock::now() should return.
+static std::deque<std::chrono::system_clock::time_point> mock_time_points;
+
+struct MockClock {
+  using duration = std::chrono::system_clock::duration;
+  using time_point = std::chrono::system_clock::time_point;
+
+  static time_point now() {
+    BOOST_REQUIRE(!mock_time_points.empty());
+    time_point t = mock_time_points.front();
+    mock_time_points.pop_front();
+    return t;
+  }
+};
+
+using TestTimer = dp3::common::BaseTimer<MockClock>;
+
+// Epsilon value for comparing double values in BOOST_CHECK_CLOSE.
+const double kEpsilon = 1e-6;
+
+}  // namespace
 
 BOOST_AUTO_TEST_SUITE(timer)
 
@@ -23,17 +43,26 @@ static_assert(!(std::is_copy_constructible<ScopedTime>::value ||
               "The class is designed to be non-copyable and non-movable");
 }  // namespace TestStaticAssert
 
-static void Delay(dp3::common::NSTimer& timer, const int delay) {
-  const dp3::common::NSTimer::StartStop scoped_time(timer);
-  std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+static void Delay(TestTimer& timer, const int delay) {
+  const MockClock::time_point now = MockClock::time_point::clock::now();
+  mock_time_points.push_back(now);
+  mock_time_points.push_back(now + std::chrono::milliseconds(delay));
+  { const TestTimer::StartStop scoped_time(timer); }
+  BOOST_CHECK(mock_time_points.empty());
 }
 
-BOOST_AUTO_TEST_CASE(timer_start_stop, *boost::unit_test::label("timer")) {
-  dp3::common::NSTimer timer;
+BOOST_AUTO_TEST_CASE(timer_constructor) {
+  TestTimer timer;
+  BOOST_CHECK_EQUAL(timer.getElapsed(), 0.0);
+  BOOST_CHECK_EQUAL(timer.getCount(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(timer_start_stop) {
+  TestTimer timer;
   {
     Delay(timer, 250);
 
-    BOOST_CHECK_CLOSE(timer.getElapsed(), 0.25, 1);
+    BOOST_CHECK_CLOSE(timer.getElapsed(), 0.25, kEpsilon);
     BOOST_CHECK_EQUAL(timer.getCount(), 1);
     BOOST_CHECK_EQUAL(timer.getElapsed(), timer.getAverage());
   }
@@ -41,20 +70,31 @@ BOOST_AUTO_TEST_CASE(timer_start_stop, *boost::unit_test::label("timer")) {
   {
     Delay(timer, 50);
 
-    BOOST_CHECK_CLOSE(timer.getElapsed(), 0.3, 1);
+    BOOST_CHECK_CLOSE(timer.getElapsed(), 0.3, kEpsilon);
     BOOST_CHECK_EQUAL(timer.getCount(), 2);
-    BOOST_CHECK_CLOSE(timer.getAverage(), 0.15, 1);
+    BOOST_CHECK_CLOSE(timer.getAverage(), 0.15, kEpsilon);
   }
   {
     Delay(timer, 300);
 
-    BOOST_CHECK_CLOSE(timer.getElapsed(), 0.6, 1);
+    BOOST_CHECK_CLOSE(timer.getElapsed(), 0.6, kEpsilon);
     BOOST_CHECK_EQUAL(timer.getCount(), 3);
-    BOOST_CHECK_CLOSE(timer.getAverage(), 0.2, 1);
+    BOOST_CHECK_CLOSE(timer.getAverage(), 0.2, kEpsilon);
   }
 }
 
-static std::string print(const dp3::common::NSTimer& timer) {
+BOOST_AUTO_TEST_CASE(timer_add) {
+  TestTimer timer1;
+  TestTimer timer2;
+  Delay(timer1, 200);
+  Delay(timer2, 50);
+  timer1 += timer2;
+  BOOST_CHECK_CLOSE(timer1.getElapsed(), 0.25, kEpsilon);
+  BOOST_CHECK_EQUAL(timer1.getCount(), 2);
+  BOOST_CHECK_CLOSE(timer1.getAverage(), 0.125, kEpsilon);
+}
+
+static std::string Print(const TestTimer& timer) {
   std::stringstream sstr;
   // Ensure the test doesn't depend on the system's locale settings.
   sstr.imbue(std::locale::classic());
@@ -62,90 +102,58 @@ static std::string print(const dp3::common::NSTimer& timer) {
   return sstr.str();
 }
 
-BOOST_AUTO_TEST_CASE(timer_print, *boost::unit_test::label("timer")) {
-  dp3::common::NSTimer timer;
+BOOST_AUTO_TEST_CASE(timer_print) {
+  TestTimer timer;
   {
     Delay(timer, 250);
-    BOOST_CHECK(std::regex_match(
-        print(timer), std::regex{"timer: avg =  25[0-9] ms, total =  "
-                                 "25[0-9] ms, count =         1"}));
+    BOOST_CHECK_EQUAL(
+        Print(timer),
+        "timer: avg =  250 ms, total =  250 ms, count =         1");
   }
-
   {
     Delay(timer, 50);
-    BOOST_CHECK(std::regex_match(
-        print(timer), std::regex{"timer: avg =  1[5-6][0-9] ms, total =  "
-                                 "3[0-1][0-9] ms, count =         2"}));
+    BOOST_CHECK_EQUAL(
+        Print(timer),
+        "timer: avg =  150 ms, total =  300 ms, count =         2");
   }
   {
     Delay(timer, 300);
-    BOOST_CHECK(std::regex_match(
-        print(timer), std::regex{"timer: avg =  2[0-1][0-9] ms, total =  "
-                                 "6[0-2][0-9] ms, count =         3"}));
+    BOOST_CHECK_EQUAL(
+        Print(timer),
+        "timer: avg =  200 ms, total =  600 ms, count =         3");
   }
 }
 
-BOOST_AUTO_TEST_CASE(timer_print_unused, *boost::unit_test::label("timer")) {
-  dp3::common::NSTimer timer;
-  BOOST_CHECK_EQUAL(print(timer), "timer: not used");
+BOOST_AUTO_TEST_CASE(timer_print_unused) {
+  TestTimer timer;
+  BOOST_CHECK_EQUAL(Print(timer), "timer: not used");
 }
 
-BOOST_AUTO_TEST_CASE(timer_print_named, *boost::unit_test::label("timer")) {
-  dp3::common::NSTimer timer{"foo"};
+BOOST_AUTO_TEST_CASE(timer_print_named) {
+  TestTimer timer{"foo"};
   Delay(timer, 100);
-  BOOST_CHECK(std::regex_match(
-      print(timer), std::regex{"foo                      : avg =  10[0-9] ms, "
-                               "total =  10[0-9] ms, count =         1"}));
+  BOOST_CHECK_EQUAL(Print(timer),
+                    "foo                      : avg =  100 ms, "
+                    "total =  100 ms, count =         1");
 }
 
-template <class T>
-static void Delay(T& accumulator, const int delay) {
-  using ScopedTime = dp3::common::ScopedMicroSecondAccumulator<T>;
-  const ScopedTime scoped_time(accumulator);
-  std::this_thread::sleep_for(std::chrono::milliseconds(delay));
-}
+BOOST_AUTO_TEST_CASE(scoped_micro_second_accumulator) {
+  struct MockAccumulator {
+    void operator+=(double d) { accumulated.push_back(d); }
+    std::vector<double> accumulated;
+  } accumulator;
 
-BOOST_AUTO_TEST_CASE(scoped_micro_second_accumumator_start_zero,
-                     *boost::unit_test::label("timer")) {
-  int accumulator = 0;
-  Delay(accumulator, 1000);
+  const MockClock::time_point now = MockClock::time_point::clock::now();
+  mock_time_points.push_back(now);
+  mock_time_points.push_back(now + std::chrono::milliseconds(42));
+  {
+    const dp3::common::ScopedMicroSecondAccumulator<MockAccumulator, MockClock>
+        scoped_time(accumulator);
+  }
+  BOOST_CHECK(mock_time_points.empty());
 
-  // Note the timer is known to be not too accurate.
-  BOOST_CHECK_CLOSE(accumulator, 1e6, 5);
-}
-
-BOOST_AUTO_TEST_CASE(scoped_micro_second_accumumator_start_non_zero,
-                     *boost::unit_test::label("timer")) {
-  int accumulator = 10000;
-  Delay(accumulator, 1);
-
-  // Note the timer is known to be not too accurate.
-  BOOST_CHECK_CLOSE(accumulator, 11e3, 5);
-}
-
-// Uses const int* since that's the iterator type of a std::aray<int, N>.
-static int RunAllDelaysInThreads(const int* first, const int* last) {
-  std::atomic<int> accumulator{0};
-  std::vector<std::thread> threads;
-  std::for_each(first, last, [&](const int delay) {
-    threads.emplace_back([&accumulator, delay] { Delay(accumulator, delay); });
-  });
-  std::for_each(threads.begin(), threads.end(),
-                [](std::thread& thread) { thread.join(); });
-
-  return accumulator;
-}
-
-BOOST_AUTO_TEST_CASE(scoped_micro_second_accumumator_atomic_integral,
-                     *boost::unit_test::label("timer")) {
-  constexpr std::array<int, 12> delays{5, 5, 3, 4, 2, 1, 1, 3, 4, 2, 2, 1};
-  const double expected =
-      1e3 * std::accumulate(delays.begin(), delays.end(), 0);
-
-  const double result = RunAllDelaysInThreads(delays.begin(), delays.end());
-
-  // Note the timer is known to be not too accurate.
-  BOOST_CHECK_CLOSE(result, expected, 5);
+  BOOST_REQUIRE_EQUAL(accumulator.accumulated.size(), std::size_t(1));
+  BOOST_CHECK_CLOSE(accumulator.accumulated.front(), 42000.0, kEpsilon);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
