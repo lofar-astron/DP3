4640f2a948a7c28a32d4639c4059584b91466127
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2023-03-29 08:21:59 +0000

    AST-1178 ColumnReader: Use unique DPBuffer; Use xtensor; Remove operation

--- steps/test/unit/tMsColumnReader.cc
+++ steps/test/unit/tMsColumnReader.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2022 ASTRON (Netherlands Institute for Radio Astronomy)
+// Copyright (C) 2023 ASTRON (Netherlands Institute for Radio Astronomy)
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "../../MsColumnReader.h"
@@ -15,20 +15,10 @@ using dp3::steps::Step;
 
 BOOST_AUTO_TEST_SUITE(column_reader)
 
-BOOST_AUTO_TEST_CASE(fields_default) {
+BOOST_AUTO_TEST_CASE(fields) {
   dp3::common::ParameterSet parset;
   const MsColumnReader column_reader(parset, "");
   BOOST_TEST(column_reader.getRequiredFields() == dp3::common::Fields());
-  BOOST_TEST(column_reader.getProvidedFields() == Step::kDataField);
-}
-
-BOOST_DATA_TEST_CASE(fields_add_subtract,
-                     boost::unit_test::data::make({"add", "subtract"}),
-                     operation) {
-  dp3::common::ParameterSet parset;
-  parset.add("operation", operation);
-  const MsColumnReader column_reader(parset, "");
-  BOOST_TEST(column_reader.getRequiredFields() == Step::kDataField);
   BOOST_TEST(column_reader.getProvidedFields() == Step::kDataField);
 }
 
